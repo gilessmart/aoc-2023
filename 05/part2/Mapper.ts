9e2779832a4274a95561f607b6de7222a9508b97
Giles Smart <gilessmart@hotmail.com>
2023-12-16 12:29:45 +0000

    further perf improvement - cache the most recent mapping

--- 05/part2/Mapper.ts
+++ 05/part2/Mapper.ts
@@ -2,21 +2,29 @@ import Mapping from "./Mapping";
 
 export default class {
     readonly mappings: Mapping[];
+    mostRecentMapping: Mapping;
 
     constructor(mappings: Mapping[]) {
         this.mappings = mappings.sort((a, b) => {
             return a.srcRangeStart - b.srcRangeStart;
         });
+        this.mostRecentMapping = mappings[0];
     }
 
     map(src: number): number {
+        if (src >= this.mostRecentMapping.srcRangeStart && src <= this.mostRecentMapping.srcRangeEnd)
+            return this.mostRecentMapping.dstSrcOffset + src
+        
         for (const mapping of this.mappings) {
             if (src < mapping.srcRangeStart)
                 return src;
             if (src > mapping.srcRangeEnd)
                 continue;
+            
+            this.mostRecentMapping = mapping;
             return mapping.dstSrcOffset + src;
         }
+
         return src;
     }
 }
